df6c163a28415115bbea54f4aa9187e588e8db19
alfer-bart <aaron.alfer@protonmail.com>
2023-02-11 20:05:23 +0300

    Added mask link

--- Assets/Scripts/TouchErase.cs
+++ Assets/Scripts/TouchErase.cs
@@ -6,9 +6,11 @@ public class TouchErase : MonoBehaviour
     public const float SqrMaskRadius = 0.140625f;
 
     [SerializeField] private SpriteMask maskPrefab;
+    [SerializeField] private SpriteMask linkPrefab;
     [SerializeField] private float spawnRate = 1f;
 
     private Vector3 lastPos;
+    private float lastTime;
 
     private void Update()
     {
@@ -29,8 +31,24 @@ public class TouchErase : MonoBehaviour
         {
             SpriteMask mask = Instantiate(maskPrefab, touchPos, Quaternion.identity);
             mask.transform.parent = transform;
+
+            if (lastTime != 0f && Time.time - lastTime < 0.15f)
+                SpawnLink(lastPos, touchPos);
+
             lastPos = touchPos;
+            lastTime = Time.time;
             Wall.OnMaskSpawn(touchPos);
         }
+    }
+
+    private void SpawnLink(Vector3 point1, Vector3 point2)
+    {
+        Vector3 diff = point2 - point1;
+        Vector3 midpoint = point1 + diff / 2;
+        float angle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        SpriteMask link = Instantiate(linkPrefab, midpoint, Quaternion.Euler(0f, 0f, angle));
+        link.transform.parent = transform;
+        link.transform.localScale = new Vector3(diff.magnitude, 0.75f, 0.75f);
     }
 }
